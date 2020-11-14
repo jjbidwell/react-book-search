1f10d092a9e50a2d80c717ed7822795591006fd4
Joshua Bidwell <jjbidwell262@gmail.com>
2020-11-14 15:12:23 -0800

    Fixed the crashing issue, but now if a book has more than one author, it mashes the names together

--- src/componenents/Searchresults/searchresults.js
+++ src/componenents/Searchresults/searchresults.js
@@ -3,29 +3,32 @@ import "../Searchresults/searchresults.css"
 
 
 function Searchresults (props) {
-    //console.log(props.results);
-if(props.results.length>5) {    
-    return (
-        <div>
-            <h1>Results:</h1>
-            {props.results.map((element, index) => {
-                console.log(element.volumeInfo)
-                return  <div key={index} className="card">
-                        <div className="card-body">
-                            <img className="thumbnail" alt={element.volumeInfo.title} src={element.volumeInfo.imageLinks.thumbnail}/>
-                            <h5 className="card-title">{element.volumeInfo.title}</h5>
-                            <p className="card-text">By {element.volumeInfo.authors[0]} </p>
-                            <button className="btn btn-primary save-btn">Save to favorites</button>
-                        </div>
-                    </div>
-        
-            })}
-        </div>
-    )
-} else {
-    return(
-        <div></div>
-    )
-}}
+        if(props.results.length===10) {    
+            return (
+                <div>
+                    <h1>Results:</h1>
+                    {props.results.map((element, index) => {
+                        //console.log(element.volumeInfo)
+                        return  <div key={index} className="card">
+                                <div className="card-body">
+                                    <img className="thumbnail" alt={element.volumeInfo.title} src={element.volumeInfo.imageLinks.thumbnail}/>
+                                    <h5 className="card-title">{element.volumeInfo.title}</h5>
+                                    <p className="card-text">By {element.volumeInfo.authors} </p>
+                                    <button className="btn btn-primary save-btn">Save to favorites</button>
+                                </div>
+                            </div>
+                
+                    })}
+                </div>
+            )
+        } else {
+            return(
+                <div></div>
+            )
+        }
+    
+    console.log(props.results);
+
+}
 
 export default Searchresults;
